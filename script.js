dbf8e9bcbe26278d20001164c5b5371861fa8f21
Student <LAB-101@aptech-msg.edu>
2024-01-01 23:40:30 +0500

    save to repo

---
+++ script.js
@@ -0,0 +1,118 @@
+let athletes = [
+    {},
+    {
+        image : "Images/athletes-sean.webp",
+        name: "Sean McColl",
+        detail : "Sean McColl is a professional rock climber from North Vancouver, Canada. In competition climbing, he competes in the competition lead climbing, competition speed climbing, and competition bouldering disciplines, and has won major competitions in all three.",
+        born:"September 3, 1987 (age 36 years), North Vancouver, Canada",
+        height:"1.69 m",
+        Nationality:"Canadian",
+        Weight:"60 kg (132 lb)",
+        achievments:"Since no other youth competitor has won more titles, this achievement is unsurpassed in youth climbing history even to this day. As an open competitor, Sean has won 33 world cup medals, of which 5 were gold, 14 were silver, and 14 were bronze."
+    },
+    {
+        image : "Images/yuzuru.webp",
+        name: "Yuzuru Hanyu",
+        detail : "Yuzuru Hanyu is a Japanese figure skater and ice show producer. He is a two-time Olympic champion, a two-time World champion, a four-time Grand Prix Final champion, the 2020 Four Continents champion",
+        born:" December 7, 1994 (age 28 years), Sendai, Miyagi, Japan",
+        height:"1.72 m",
+        Nationality:"Japenese",
+        Weight:"57 kg",
+        achievments:"Throughout his career, Hanyu has won the Olympics twice (2014, 2018) and achieved a Super Slam for having won all major competitions in his senior and junior"
+    },
+    {
+        image : "Images/cocogauff.webp",
+        name: "Coco Gauff",
+        detail : "Cori Dionne 'Coco' Gauff is an American professional tennis player. She has won six WTA Tour singles titles, including a Major at the 2023 US Open, and eight doubles titles. Gauff has career-high rankings of world No. 3 in singles and of world No. 1 in doubles by the Women's Tennis Association.",
+        born:" March 13, 2004 (age 19 years), Atlanta, Georgia, United States",
+        height:"1.75 m",
+        Nationality:"American",
+        Weight:" 55 kg",
+        achievments:"She has won six WTA Tour singles titles, including a Major at the 2023 US Open, and eight doubles titles. Gauff has career-high rankings of world No. 3 in singles and of world No. 1 in doubles by the Women's Tennis Association (WTA)."
+    },
+    {
+        image : "Images/marcusd'almeida.webp",
+        name: "Marcus D'almeida",
+        detail : "Marcus Vinicius Carvalho Lopes D'Almeida, known as Marcus D'Almeida, is a Brazilian athlete who competes in recurve archery. World runner-up in 2021. In February 2023, he became the first Brazilian to lead the world archery ranking in his category, the recurve bow.",
+        born:"January 30, 1998 (age 25 years), Rio de Janeiro, State of Rio de Janeiro, Brazil",
+        height:"1.86 m",
+        Nationality:"Brazilian",
+        Weight:"90 kg",
+        achievments:"Marcus Vinicius Carvalho Lopes D'Almeida known as Marcus D'Almeida, is a Brazilian athlete who competes in recurve archery. World runner-up in 2021."
+    },
+    {
+        image : "Images/flipe.webp",
+        name: "Flipe Toledo",
+        detail : "Filipe Toledo is a Brazilian professional surfer who has competed on the World Surf League Men's World Tour since 2013. He’s a second-generation pro who grew up outside of Ubatuba, Brasil on the northeastern coast of the state of São Paulo. His father, and long-time coach, Ricardo, was a two-time national champion.",
+        born:"April 16, 1995 (age 28 years), Ubatuba, State of São Paulo, Brazil",
+        height:"1.75 m",
+        Nationality:"Brazilian",
+        Weight:"154 lb (70 kg)",
+        achievments:"2x World Champion (2022, 2023) WSL Championship Tour event wins: 17. 2x US Open of Surfing champion (2014, 2016)"
+    },
+    {
+        image : "Images/katieledecky.webp",
+        name: "Katie Ledecky",
+        detail : "Kathleen Genevieve Ledecky is an American competitive swimmer. She has won seven Olympic gold medals and 21 world championship gold medals, the most in history for a female swimmer. She has won a world record 16 individual gold medals at the World Aquatics Championships.",
+        born:"March 17, 1997 (age 26 years), Washington, D.C., United States",
+        height:"1.83 m",
+        Nationality:"American",
+        Weight:"73 kg",
+        achievments:"Katie Ledecky is the most decorated of female swimmer in history with six individual Olympic gold medals and 14 individual long course world titles to date."
+    },
+    {
+        image : "Images/kayleemckeown.webp",
+        name: "Kaylee Mckeown",
+        detail : "Kaylee Rochelle McKeown OAM is an Australian swimmer and triple Olympic gold medalist. She is the world record holder in the long course 50 metre backstroke, 100 metre backstroke and both the long course and short course 200 metre backstroke.",
+        born:"July 12, 2001 (age 22 years), Redcliffe, Australia",
+        height:" 1.75 m",
+        Nationality:"Australian",
+        Weight:"60 kg (132 lb)",
+        achievments:"Kaylee McKeown first appeared on the Olympic stage at the Buenos Aires 2018 Youth Olympics, where the swimmer won women's 50m backstroke gold, alongside silver in the 100m backstroke, and bronze in the 200m backstroke."
+    },
+    {
+        image : "Images/yuto.webp",
+        name: "Yuto Horigome",
+        detail : "Yuto Horigome is a Japanese professional skateboarder. He won the gold medal in the inaugural Olympic men's street event at the 2020 Summer Olympics, becoming the first person ever to win a gold medal in skateboarding at the Olympics.",
+        born:"January 7, 1999 (age 24 years), Koto City, Tokyo, Japan",
+        height:"1.7 m",
+        Nationality:"Japanese",
+        Weight:"55 kg",
+        achievments:"Yuto Horigome is a Japanese professional skateboarder. He won the gold medal in the inaugural Olympic men's street event at the 2020 Summer "
+    },
+    {
+        image : "Images/flora.webp",
+        name: "Flora Duffy",
+        detail : "Dame Flora Jane Duffy DBE is a Bermudian triathlete. She won a gold medal at the 2020 Summer Olympics in Tokyo, Bermuda's first gold medal. She also competed in Beijing, London, and Rio de Janeiro Olympics. In 2018, she won gold in the women's triathlon at the XXI Commonwealth Games in Australia.",
+        born:" September 30, 1987 (age 36 years), Paget Parish, Bermuda",
+        height:"1.69 m",
+        Nationality:"Bermudian, British",
+        Weight:"58 kg",
+        achievments:"Dame Flora Jane Duffy DBE (born 30 September 1987) is a Bermudian triathlete. She won a gold medal at the 2020 Summer Olympics in Tokyo,"
+    },
+    {
+        image : "Images/alaamaso.webp",
+        name: "Alla Maso",
+        detail : "He competed at the 2020 Summer Olympics for the Refugee Olympic Team, taking part in the men's 50m freestyle.His brother Mohamad Maso competed for Syria in the men's triathlon at the 2020 Olympics, coming 47th. The brothers embraced during the Parade of Nations in the opening ceremony.The two brothers left Syria as refugees in 2015 and settled in Germany in 2016.",
+        born:"1 January 2000",
+        height:"184 cm",
+        Nationality:"Canadian",
+        Weight:"60 kg (132 lb)",
+        achievments:"Since no other youth competitor has won more titles, this achievement is unsurpassed in youth climbing history even to this day. As an open competitor, Sean has won 33 world cup medals, of which 5 were gold, 14 were silver, and 14 were bronze."
+    },
+    
+]
+let atheletecontainer = document.getElementById("athcon");
+for(let i = 1; i<athletes.length; i++){
+    atheletecontainer.innerHTML += `
+    <div class="card athcard p-2 pt-4" style="width: 18rem;">
+  <img src="${ athletes[i].image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${athletes[i].name}</h5>
+    <hr>
+    <a href="athlete-detail.html?id=${i}"><button class="btn btn-primary btnath">Explore </button></a>
+  </div>
+</div>
+    
+    `
+}
